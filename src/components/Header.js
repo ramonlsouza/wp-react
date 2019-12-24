b84c7e8b2135cfadce70a01193025ee7da7c494f
Ramón Souza <contato@ramonsouza.com>
2019-12-24 13:54:24 -0300

    responsiveness tweaks

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,14 @@ class Header extends Component {
       return(
         <div className="header">
           <div className="container">
+            <span className="header-title">Materiais gratuitos <span role="img" aria-label="smiley face emoji">😀</span></span>
+
             <button onClick={() => this.props.handleFilter('all')} className={"category " + (this.props.activeCategory === 'all' ? 'active' : '')}><img src="/icons/stack.svg" alt="stack icon" /> Todos</button> 
             <button onClick={() => this.props.handleFilter('o_e-book')} className={"category " + (this.props.activeCategory === 'o_e-book' ? 'active' : '')}><img src="/icons/book.svg" alt="book icon" /> E-books</button> 
             <button onClick={() => this.props.handleFilter('o_infográfico')} className={"category " + (this.props.activeCategory === 'o_infográfico' ? 'active' : '')}><img src="/icons/chart.svg" alt="chart icon" /> Infográficos</button> 
             <button onClick={() => this.props.handleFilter('o_webinar')} className={"category " + (this.props.activeCategory === 'o_webinar' ? 'active' : '')}><img src="/icons/play.svg" alt="play icon" /> Webinar</button> 
             <button onClick={() => this.props.handleFilter('a_ferramenta')} className={"category " + (this.props.activeCategory === 'a_ferramenta' ? 'active' : '')}><img src="/icons/gear.svg" alt="gear icon" /> Ferramentas</button> 
 
-            <span className="header-title">Materiais gratuitos <span role="img" aria-label="smiley face emoji">😀</span></span>
           </div>
         </div>
       )
